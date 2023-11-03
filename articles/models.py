59193b0947d44b6586934c3975bdf08b1b0de8cf
yootina <tinayoo1004@gmail.com>
2023-11-03 10:29:12 +0900

    Read 전체 기능 구현

--- articles/models.py
+++ articles/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=50)
-    content = models.TextField
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
+
+def index(request):
+    articles = Article.objects.all()
+
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'index.html', context)
